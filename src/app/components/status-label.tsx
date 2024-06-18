2a052caa142252d5b29b3262639d9cb15c83ea1f
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-18 11:02:09 +0300

    added tailwindcss and clsx

---
+++ src/app/components/status-label.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export enum Status {
+  Active = 'active',
+  NotActive = 'notActive',
+  Pending = 'pending',
+  Suspended = 'suspended',
+}
+export interface StatusLableProps {
+  children: React.ReactNode;
+  status: Status;
+  disabled?: boolean;
+}
+
+export default function StatusLable({
+  children,
+  status,
+  disabled,
+}: StatusLableProps) {
+  return (
+    <div
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+        status === Status.Active && 'text-green-780 bg-green-100',
+        status === Status.NotActive && 'text-red-780 bg-red-100',
+        status === Status.Pending && 'text-orange-780 bg-orange-100',
+        status === Status.Suspended && 'text-blue-780 bg-blue-100',
+        { ['opacity-75 cursor-not-allowed']: disabled },
+      )}
+    >
+      <div className="w-1 h-1 mr-2 rounded-full bg-current" />
+      {children}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLable, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className="text-4xl font-bold ">Home Page</h1>
+      <StatusLable status={Status.Active}>Active</StatusLable>
+      <StatusLable status={Status.NotActive}>Not Active</StatusLable>
+      <StatusLable status={Status.Pending}>Pending</StatusLable>
+      <StatusLable status={Status.Suspended}>Suspended</StatusLable>
     </main>
   );
 }
