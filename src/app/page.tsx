fca7c6096e72ab19d15ad9ae12b318b4ee393cb2
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-19 22:06:00 +0300

    added @headlessui/react and modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { headers } from 'next/headers';
-import StatusLable, { Status } from './components/status-label';
-import Button from './components/button';
+import AddCompanyButton from '@/app/components/add-company-button';
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-bold ">Home Page</h1>
-      <StatusLable status={Status.Active}>Active</StatusLable>
-      <StatusLable status={Status.NotActive}>Not Active</StatusLable>
-      <StatusLable status={Status.Pending}>Pending</StatusLable>
-      <StatusLable status={Status.Suspended}>Suspended</StatusLable>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
