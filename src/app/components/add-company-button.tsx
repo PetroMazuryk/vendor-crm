3d90f94725c502c18e6100cf9df67ee4ab6cf894
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-19 22:43:59 +0300

    added dynamic import

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -3,8 +3,11 @@
 import React, { useState } from 'react';
 
 import Button from '@/app/components/button';
+import dynamic from 'next/dynamic';
 
-import CompanyFormModal from '@/app/components/company-form-modal';
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
 
 export default function AddCompanyButton() {
   const [show, setShow] = useState(false);
