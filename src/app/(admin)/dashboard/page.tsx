34b799bc9ac593cd255787caf5f7c805ab942497
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-27 00:30:29 +0300

    added sidebar

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
     <main>
-      <h1 className="text-xl">Settings Page</h1>
+      <h1 className="text-xl">Dashboard Page</h1>
     </main>
   );
 }
