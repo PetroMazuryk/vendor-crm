b496113e38896a40541dbc427e022bc0ee860890
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-29 12:15:26 +0300

    added not-found.tsx file

---
+++ src/app/(admin)/companies/[id]/not-found.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+
+export interface NotFoundPropos {}
+
+export default function NotFound({}: NotFoundPropos) {
+  return (
+    <div>
+      <p>Could not found company</p>
+      <Link href="/companies" className="text-blue-500">
+        Back to companies
+      </Link>
+    </div>
+  );
+}
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import MagicButton from '@/app/components/magic-button';
+// import MagicButton from '@/app/components/magic-button';
 import React from 'react';
 
 export interface PageProps {}
@@ -7,7 +7,7 @@ export default function Page({}: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
+      {/* <MagicButton /> */}
     </main>
   );
 }
