4cf1f5b1f735d6624f559c2ba3e2275db7568ca7
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-30 20:21:38 +0300

    added countries of companies

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -4,18 +4,21 @@ export interface LayoutProps {
   children: React.ReactNode;
 
   categories: React.ReactNode;
+  countries: React.ReactNode;
 }
 
 export default function Layout({
   children,
 
   categories,
+  countries,
 }: LayoutProps) {
   return (
     <div>
       {children}
       <main className="grid grid-cols-12 gap-5 py-10 pl-10 pr-7">
         <div className="col-span-7">{categories}</div>
+        <div className="col-span-6">{countries}</div>
       </main>
     </div>
   );
